71aa6bb18790b9af4789400505077a960b75e94d
Dima <pushokf@gmail.com>
2024-01-18 14:28:54 +0200

    login user in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,7 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
     });
 
     // Generate token
-    const token = generateToken(user._id)
+    const token = generateToken(user._id);
 
     if (user) {
         const { _id, name, email, role } = user;
@@ -67,6 +67,49 @@ const registerUser = asyncHandler(async (req, res) => {
     res.send("Register a New User!");
 });
 
+// Login existing user
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    // Validate request
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please add email and password.");
+    }
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+        res.status(400);
+        throw new Error("User does not exist!");
+    }
+
+    // User exists, now check if password is correct
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    // Generate token
+    const token = generateToken(user._id);
+    if (user && passwordIsCorrect) {
+        const newUser = await User.findOne({ email }).select("-password");
+        res.cookie("token", token, {
+            path: "/",
+            httpOnly: true,
+            expires: new Date(Date.now() + 1000 * 86400),
+            // secure: true,
+            // sameSite: none, // for backend and frontend set to true?
+        });
+
+        // Send user data to frontend
+        res.status(201).json(newUser);
+    } else {
+        res.status(400);
+        throw new Error("Invalid email or password!");
+    }
+
+
+});
+
 module.exports = {
     registerUser,
+    loginUser,
 };
