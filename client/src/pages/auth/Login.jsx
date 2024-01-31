e842653ef469ef81c49defafd0d25976747d4aab
Dima <pushokf@gmail.com>
2024-01-31 17:02:46 +0200

    login user redux component logic and functionality

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,17 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./auth.module.scss";
 import loginImg from "../../assets/login.png";
 import Card from "../../components/card/Card";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { validateEmail } from "../../redux/features/auth/authService";
+import Loader from "../../components/loader/Loader";
+import { useDispatch, useSelector } from "react-redux";
+import { RESET_AUTH, login } from "../../redux/features/auth/authSlice";
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { isLoading, isLoggedIn, isSuccess } = useSelector((state) => state.auth);
 
-    const loginUser = () => { };
+    const loginUser = async (e) => { 
+        e.preventDefault();
+        if (!email || !password) {
+            return toast.error("All fields are required!");
+        }
+        if (!validateEmail(email)) {
+            return toast.error("Please enter a valid email.");
+        }
+        const userData = {
+            email,
+            password,
+        }
+
+        // console.log(userData);
+        await dispatch(login(userData));
+    };
+
+    useEffect(() => {
+        if (isSuccess && isLoggedIn) {
+            navigate("/");
+        }
+
+        dispatch(RESET_AUTH())
+    }, [isSuccess, isLoggedIn, dispatch, navigate]);
 
     return (
         <>
+            {isLoading && <Loader />}
             <section className={`container ${styles.auth}`}>
                 <div className={styles.img}>
                     <img src={loginImg} alt="login" width="400" />
